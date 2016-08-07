664bcd9dbb880343fc133718b32581c92bb09407
Jeff <github@314ghz.com>
2016-08-06 20:02:42 -0500

    Did some work on the selection sort. Also took out the AbstractSort class for now due to some problems with the code.

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
-all: MainTest
+all: SelectionSort.o SortingHandler
 
-MainTest: MainTest.cpp
-	g++ -std=c++11 -Wall -o MainTest MainTest.cpp
+SortingHandler: SortingHandler.cpp SelectionSort.o
+	g++ -std=c++11 -Wall -o SortingHandler SortingHandler.cpp
+
+SelectionSort.o: SelectionSort.cpp
+	g++ -std=c++11 -Wall -c -o SelectionSort.o SelectionSort.cpp
 
 clean:
-	if [ -e MainTest ]; then rm MainTest; fi;
+	if [ -e SortingHandler ]; then rm SortingHandler; fi;
 	if [ -e *~ ]; then rm *~; fi;
 	if [ -e *.o ]; then rm *.o; fi;
 	if [ -e *.gch ]; then rm *.gch; fi;
--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,19 +1,67 @@
-#include "SelectionSort.h"
+#include <iostream>
+#include <vector>
+#include <random>
+#include <fstream>
 
-template <class T> std::vector<T> sort( std::vector<T> list )
+template <class T> class SelectionSort
 {
-	int length = list.length;
-	for( int i = 0; i < length; i++ )
-	{
-		int min = i;
-		for( int j = i+1; j < length; j++ )
+	private:
+		static bool less( T first, T second )
 		{
-			if( less(list[j], list[min]) )
-			{
-				min = j;
-			}
+			return first < second;
 		}
-		exch( list, i, min );
-	}
-	return list;
-}
+
+		static void exch( std::vector<T> &list, int first, int second )
+		{
+			T temp = list[first];
+			list[first] = list[second];
+			list[second] = temp;
+		}
+
+		static void show( std::vector<T> list )
+		{
+			for( int i = 0; i < list.size(); i++ )
+			{
+				std::cout << list[i] << " ";
+			}
+			std::cout << std::endl;
+		}
+
+	protected:
+
+	public:
+		std::vector<T> sort( std::vector<T> &list )
+		{
+			int length = list.size();
+			for( int i = 0; i < length; i++ )
+			{
+				int min = i;
+				for( int j = i+1; j < length; j++ )
+				{
+					if( list[j] < list[min] )
+					{
+						min = j;
+					}
+				}
+				exch( list, i, min );
+			}
+			return list;
+		}
+
+		static bool isSorted( std::vector<T> list )
+		{
+			if( list.size() == 0 )
+			{
+				return false;
+			}
+
+			for( int i = 1; i < list.size(); i++ )
+			{
+				if( less(list[i], list[i-1]) )
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+};
