cb0a6ad150d8d98d057ccf454ecebd3537d4003a
Jeff Crone <github@314ghz.com>
2016-08-09 15:09:56 -0400

    Added an abstract Java class.

--- AbstractSort.java
+++ AbstractSort.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.lang.Integer;
 
-public class SelectionSort<T extends Comparable<T>>
+public abstract class AbstractSort<T extends Comparable<T>>
 {
 	/**
 	 * Checks if one value is less than another.
@@ -46,28 +46,12 @@ public class SelectionSort<T extends Comparable<T>>
 	}
 
 	/**
-	 * Sorts the given list using selection sort.
+	 * Sorts the given list.
 	 *
 	 * @param list The list to sort.
 	 * @return The sorted list.
 	 */
-	public List<T> sort( List<T> list )
-	{
-		int length = list.size();
-		for( int i = 0; i < length; i++ )
-		{
-			int min = i;
-			for( int j = i+1; j < length; j++ )
-			{
-				if( less(list.get(j), list.get(min)) )
-				{
-					min = j;
-				}
-			}
-			exch( list, i, min );
-		}
-		return list;
-	}
+	public abstract List<T> sort( List<T> list );
 
 	/**
 	 * Checks if the list is sorted.
@@ -90,45 +74,5 @@ public class SelectionSort<T extends Comparable<T>>
 			}
 		}
 		return true;
-	}
-
-	/**
-	 * Constructor.
-	 */
-	public SelectionSort()
-	{
-	}
-
-	/**
-	 * Main method. Generates some random values and runs the sort.
-	 *
-	 * @param  args The command line arguments when the program is run.
-	 */
-	public static void main( String[] args )
-	{
-		SelectionSort<Integer> selectionSort = new SelectionSort();
-		List<Integer> randomInts = generateRandomInts( 10 );
-		selectionSort.show( randomInts );
-		selectionSort.sort( randomInts );
-		selectionSort.show( randomInts );
-		//System.out.println( hw.getTextToDisplay() );
-	}
-
-	/**
-	 * Generates an ArrayList of pseudorandom integers.
-	 * 
-	 * @param num The number of integers to generate.
-	 * @return An ArrayList with num pseudorandom integers in it.
-	 */
-	private static List<Integer> generateRandomInts( int num )
-	{
-		int maxVal = 100;//Maximum value to generate.
-		List<Integer> randomInts = new ArrayList<Integer>();
-		Random rnd = new Random( java.lang.System.currentTimeMillis() );
-		for( int i = 0; i < num; i++ )
-		{
-			randomInts.add( rnd.nextInt(maxVal) );
-		}
-		return randomInts;
 	}
 }
