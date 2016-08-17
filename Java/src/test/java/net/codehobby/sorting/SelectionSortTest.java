3c0aa5f0d3f189520a5fa53514de9e472fd31270
Jeff Crone <github@314ghz.com>
2016-08-17 09:27:59 -0400

    Added another test case.

--- Java/src/test/java/net/codehobby/sorting/SelectionSortTest.java
+++ Java/src/test/java/net/codehobby/sorting/SelectionSortTest.java
@@ -29,4 +29,26 @@ public class SelectionSortTest
 		selSort.exch( testInts, 0, 1 );
 		assertTrue( "exch method should exchange two values in a list.", testInts.get(0) == 2 && testInts.get(1) == 1 );
 	}
+
+	/**
+	 * Test the exch method of AbstractSort.
+	 */
+	@Test public void testIsSortedMethod()
+	{
+		AbstractSort<Integer> selSort = new SelectionSort();
+		List<Integer> sortedInts = new ArrayList<Integer>();
+		List<Integer> nonsortedInts = new ArrayList<Integer>();
+
+		//Test isSorted on a sorted list.
+		sortedInts.add( 1 );
+		sortedInts.add( 2 );
+		sortedInts.add( 3 );
+		assertTrue( "isSorted method should return true if a list is sorted.", selSort.isSorted( sortedInts ) == true );
+
+		//Test isSorted on a list that is not sorted.
+		nonsortedInts.add( 2 );
+		nonsortedInts.add( 3 );
+		nonsortedInts.add( 1 );
+		assertTrue( "isSorted method should return false if a list is not sorted.", selSort.isSorted( nonsortedInts ) == false );
+	}
 }
