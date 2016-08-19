fcba402271e442b32dde3eb254a5fbb901bf7a18
Jeff Crone <github@314ghz.com>
2016-08-19 09:29:06 -0400

    Added a couple test cases.

--- Java/src/test/java/net/codehobby/sorting/SelectionSortTest.java
+++ Java/src/test/java/net/codehobby/sorting/SelectionSortTest.java
@@ -45,10 +45,42 @@ public class SelectionSortTest
 		sortedInts.add( 3 );
 		assertTrue( "isSorted method should return true if a list is sorted.", selSort.isSorted( sortedInts ) == true );
 
+		//Test isSorted on a sorted list with a duplicate.
+		sortedInts.clear();
+		sortedInts.add( 1 );
+		sortedInts.add( 2 );
+		sortedInts.add( 3 );
+		sortedInts.add( 3 );
+		assertTrue( "isSorted method should return true if a list is sorted with duplicates.", selSort.isSorted( sortedInts ) == true );
+
 		//Test isSorted on a list that is not sorted.
 		nonsortedInts.add( 2 );
 		nonsortedInts.add( 3 );
 		nonsortedInts.add( 1 );
 		assertTrue( "isSorted method should return false if a list is not sorted.", selSort.isSorted( nonsortedInts ) == false );
+	}
+
+	/**
+	 * Test the sort method of SelectionSort.
+	 */
+	@Test public void testSortMethod()
+	{
+		AbstractSort<Integer> selSort = new SelectionSort();
+		List<Integer> nonsortedInts = new ArrayList<Integer>();
+
+		//Test sort on a list that is not sorted.
+		nonsortedInts.add( 2 );
+		nonsortedInts.add( 3 );
+		nonsortedInts.add( 1 );
+		nonsortedInts.add( 9 );
+		nonsortedInts.add( 5 );
+		nonsortedInts.add( 4 );
+		nonsortedInts.add( 8 );
+		nonsortedInts.add( 6 );
+		nonsortedInts.add( 7 );
+
+		List<Integer> sortedInts = selSort.sort( nonsortedInts );
+
+		assertTrue( "sort method should return sort a list.", selSort.isSorted( sortedInts ) );
 	}
 }
