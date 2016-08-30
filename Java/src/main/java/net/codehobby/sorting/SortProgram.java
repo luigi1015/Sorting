898dbf8bf666811a278d8664ddf06fefd772543d
Jeff Crone <github@314ghz.com>
2016-08-30 09:06:37 -0400

    Removed some code that wasn't being used.

--- Java/src/main/java/net/codehobby/sorting/SortProgram.java
+++ Java/src/main/java/net/codehobby/sorting/SortProgram.java
@@ -27,22 +27,4 @@ public class SortProgram
 		System.out.println( "Sorted Integers:" );
 		selectionSort.show( randomInts );
 	}
-
-	/**
-	 * Generates an ArrayList of pseudorandom integers.
-	 * 
-	 * @param num The number of integers to generate.
-	 * @return An ArrayList with num pseudorandom integers in it.
-	 */
-	private static List<Integer> generateRandomInts( int num )
-	{
-		int maxVal = 100;//Maximum value to generate.
-		List<Integer> randomInts = new ArrayList<Integer>();
-		Random rnd = new Random( java.lang.System.currentTimeMillis() );
-		for( int i = 0; i < num; i++ )
-		{
-			randomInts.add( rnd.nextInt(maxVal) );
-		}
-		return randomInts;
-	}
 }
