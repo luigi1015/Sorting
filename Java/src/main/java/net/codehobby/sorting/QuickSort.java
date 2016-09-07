1ffb0399d58c7b03c5e9c7111136db6411a7dac4
Jeff <github@314ghz.com>
2016-09-06 22:02:55 -0500

    Added QuickSort and QuickSortTest classes.

---
+++ Java/src/main/java/net/codehobby/sorting/QuickSort.java
@@ -0,0 +1,26 @@
+package net.codehobby.sorting;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+import java.lang.Integer;
+
+public class QuickSort<T extends Comparable<T>> extends AbstractSort<T>
+{
+	/**
+	 * Sorts the given list using selection sort.
+	 *
+	 * @param list The list to sort.
+	 * @return The sorted list.
+	 */
+	public List<T> sort( List<T> list )
+	{
+	}
+
+	/**
+	 * Constructor.
+	 */
+	public QuickSort()
+	{
+	}
+}
---
+++ Java/src/test/java/net/codehobby/sorting/QuickSortTest.java
@@ -0,0 +1,86 @@
+package net.codehobby.sorting;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.Integer;
+
+public class QuickSortTest
+{
+	/**
+	 * Test the less method of AbstractSort.
+	 */
+	@Test public void testLessMethod()
+	{
+		AbstractSort<Integer> qSort = new QuickSort();
+		assertTrue( "less method should return 'true' if the first parameter is less than the second.", selSort.less(1, 2) );
+	}
+
+	/**
+	 * Test the exch method of AbstractSort.
+	 */
+	@Test public void testExchMethod()
+	{
+		AbstractSort<Integer> qSort = new QuickSort();
+		List<Integer> testInts = new ArrayList<Integer>();
+		testInts.add( 1 );
+		testInts.add( 2 );
+		selSort.exch( testInts, 0, 1 );
+		assertTrue( "exch method should exchange two values in a list.", testInts.get(0) == 2 && testInts.get(1) == 1 );
+	}
+
+	/**
+	 * Test the exch method of AbstractSort.
+	 */
+	@Test public void testIsSortedMethod()
+	{
+		AbstractSort<Integer> qSort = new QuickSort();
+		List<Integer> sortedInts = new ArrayList<Integer>();
+		List<Integer> nonsortedInts = new ArrayList<Integer>();
+
+		//Test isSorted on a sorted list.
+		sortedInts.add( 1 );
+		sortedInts.add( 2 );
+		sortedInts.add( 3 );
+		assertTrue( "isSorted method should return true if a list is sorted.", selSort.isSorted( sortedInts ) == true );
+
+		//Test isSorted on a sorted list with a duplicate.
+		sortedInts.clear();
+		sortedInts.add( 1 );
+		sortedInts.add( 2 );
+		sortedInts.add( 3 );
+		sortedInts.add( 3 );
+		assertTrue( "isSorted method should return true if a list is sorted with duplicates.", selSort.isSorted( sortedInts ) == true );
+
+		//Test isSorted on a list that is not sorted.
+		nonsortedInts.add( 2 );
+		nonsortedInts.add( 3 );
+		nonsortedInts.add( 1 );
+		assertTrue( "isSorted method should return false if a list is not sorted.", selSort.isSorted( nonsortedInts ) == false );
+	}
+
+	/**
+	 * Test the sort method of QuickSort.
+	 */
+	@Test public void testSortMethod()
+	{
+		AbstractSort<Integer> qSort = new QuickSort();
+		List<Integer> nonsortedInts = new ArrayList<Integer>();
+
+		//Test sort on a list that is not sorted.
+		nonsortedInts.add( 2 );
+		nonsortedInts.add( 3 );
+		nonsortedInts.add( 1 );
+		nonsortedInts.add( 9 );
+		nonsortedInts.add( 5 );
+		nonsortedInts.add( 4 );
+		nonsortedInts.add( 8 );
+		nonsortedInts.add( 6 );
+		nonsortedInts.add( 7 );
+
+		List<Integer> sortedInts = selSort.sort( nonsortedInts );
+
+		assertTrue( "sort method should return sort a list.", selSort.isSorted( sortedInts ) );
+	}
+}
